65a8ea3253f8303271a13c64186ee2e4cc81df3d
Malak Hannosh <malakh@ad.unc.edu>
2023-11-04 17:36:43 -0400

    Adding HTTPClient to Seat Service constructor.

--- frontend/src/app/seat/seat.service.ts
+++ frontend/src/app/seat/seat.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SeatService {
-
-  constructor() { }
+  constructor(protected http: HttpClient) {}
 }
