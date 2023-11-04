28c8b64ce5004dc68aacb1db3c9677f2fbe4001d
Maya Theresa Gopal <69762402+mayather@users.noreply.github.com>
2023-11-04 17:07:12 -0400

    Create seat-manage component (#27)
    * Create seat-manage component
    
    * Add link to seat-page from seat-manage page

--- frontend/src/app/seat/seat.module.ts
+++ frontend/src/app/seat/seat.module.ts
@@ -2,10 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SeatRoutingModule } from './seat-routing.module';
 import { SeatPageComponent } from './seat-page/seat-page.component';
-import { SeatManageComponent } from './seat-manage/seat-manage.component';
 
 @NgModule({
-  declarations: [SeatPageComponent, SeatManageComponent],
+  declarations: [SeatPageComponent],
   imports: [CommonModule, SeatRoutingModule]
 })
 export class SeatModule {}
