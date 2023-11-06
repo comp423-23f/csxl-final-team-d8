610a74e9bf71f0e487e35c0ce4a5fa9e51ae1cbf
Nathan Flinchum <nflinchum@unc.edu>
2023-11-06 15:12:17 -0500

    Added rooms view to navigation sidebar, Pair Programming

--- frontend/src/app/room/room-page/room-page.component.ts
+++ frontend/src/app/room/room-page/room-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { profileResolver } from 'src/app/profile/profile.resolver';
 
 @Component({
   selector: 'app-room-page',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./room-page.component.css']
 })
 export class RoomPageComponent {
-
+  /** Route information to be used in app-routing module */
+  public static Route = {
+    path: '',
+    title: 'Rooms',
+    component: RoomPageComponent,
+    canActivate: [],
+    resolve: { profile: profileResolver }
+  };
 }
