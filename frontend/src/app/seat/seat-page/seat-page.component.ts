9d4cda9c9b520851b921b15031b3feb535f5beae
Aknazar Janibek <89208624+ajanibekcode@users.noreply.github.com>
2023-11-06 15:05:50 -0500

    Merge backend branch into stage (#44)
    * Establish frontend branch
    
    * Make small formatting change to ReadMe
    
    * Create room and seat directories (#21)
    
    * Create seat module, seat routing module, and seat-page component  -- Pair Programming (#26)
    
    * Create router api for rooms and its associated service methods (#43)
    
    * Create router api for rooms and its associated service methods
    
    * Create TODO files for additional service and API route methods
    
    ---------
    
    Co-authored-by: Maya Theresa Gopal <mayather@email.unc.edu>
    Co-authored-by: Maya Theresa Gopal <69762402+mayather@users.noreply.github.com>
    Co-authored-by: Nathan Flinchum <89219510+nathanlf@users.noreply.github.com>

---
+++ frontend/src/app/seat/seat-page/seat-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-seat-page',
+  templateUrl: './seat-page.component.html',
+  styleUrls: ['./seat-page.component.css']
+})
+export class SeatPageComponent {
+
+}
