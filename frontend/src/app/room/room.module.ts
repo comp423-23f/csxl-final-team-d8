ad050891f3c07c4dd7157f88e0112387f70b7e78
Malak Hannosh <malakh@ad.unc.edu>
2023-11-05 23:15:29 -0500

    Creating room page component.

--- frontend/src/app/room/room.module.ts
+++ frontend/src/app/room/room.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { RoomRoutingModule } from './room-routing.module';
 import { RoomManageComponent } from './room-manage/room-manage.component';
+import { RoomPageComponent } from './room-page/room-page.component';
 
 
 @NgModule({
   declarations: [
-    RoomManageComponent
+    RoomManageComponent,
+    RoomPageComponent
   ],
   imports: [
     CommonModule,
