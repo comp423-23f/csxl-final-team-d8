dc7545676917b9abf02b6ebb7fbd5c96dc3c68bb
Maya Theresa Gopal <69762402+mayather@users.noreply.github.com>
2023-11-06 21:55:51 -0500

    Called seat-service in seat component (#49)

--- frontend/src/app/seat/seat-page/seat-page.component.ts
+++ frontend/src/app/seat/seat-page/seat-page.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { Seat } from '../seat.model';
+import { ActivatedRoute } from '@angular/router';
+import { SeatService } from '../seat.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-seat-page',
   templateUrl: './seat-page.component.html',
   styleUrls: ['./seat-page.component.css']
 })
-
-export class SeatPageComponent {}
-
+export class SeatPageComponent {
+  public seats: Observable<Seat[]>;
+  constructor(
+    private route: ActivatedRoute,
+    private seatService: SeatService
+  ) {
+    /** Initialize data from resolvers. */
+    const data = this.route.snapshot.data as {
+      seats: Seat[];
+    };
+    this.seats = seatService.getSeats();
+  }
+}
