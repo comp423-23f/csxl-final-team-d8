3b27ba272e7696930779f77201e7fbc99420c300
Nathan Flinchum <89219510+nathanlf@users.noreply.github.com>
2023-11-06 22:00:46 -0500

    Merging frontend to stage to finalize Sprint 0 submission (#50)
    * Establish frontend branch
    
    * Make small formatting change to ReadMe
    
    * Create room and seat directories (#21)
    
    * Create seat module, seat routing module, and seat-page component  -- Pair Programming (#26)
    
    * Create seat-manage component (#27)
    
    * Create seat-manage component
    
    * Add link to seat-page from seat-manage page
    
    * Creating Seat Service to store seat info. (#31)
    
    * Creating Seat Service to store seat info.
    
    * Adding HTTPClient to Seat Service constructor.
    
    * Created room and seat models in frontend (#36)
    
    * Create room-manage component (#28)
    
    * Adding parts 0 and 1 of design-doc (#10)
    
    * Add section 4 of design doc (#11)
    
    LGTM
    
    * Add personas and user stories to design doc (#12)
    
    * Add personas and user stories to design doc
    
    * Update design doc personas to provide more detail
    
    * Add section 5 to Design-doc (#13)
    
    * Add section 5 to Design-doc
    
    * Add revised section 5 to design-doc
    
    * Create room-manage component
    
    * Fix app module import statement for room and seat
    
    * Remove old TODO file
    
    ---------
    
    Co-authored-by: Malak Hannosh <77818528+malakhannosh@users.noreply.github.com>
    Co-authored-by: Maya Theresa Gopal <69762402+mayather@users.noreply.github.com>
    Co-authored-by: Nathan Flinchum <89219510+nathanlf@users.noreply.github.com>
    
    * Fix merge conflict from 28 PR (#37)
    
    * Adding parts 0 and 1 of design-doc (#10)
    
    * Add section 4 of design doc (#11)
    
    LGTM
    
    * Add personas and user stories to design doc (#12)
    
    * Add personas and user stories to design doc
    
    * Update design doc personas to provide more detail
    
    * Add section 5 to Design-doc (#13)
    
    * Add section 5 to Design-doc
    
    * Add revised section 5 to design-doc
    
    * Create room-manage component
    
    * Fix app module import statement for room and seat
    
    * Remove old TODO file
    
    * Fix Design-doc error from previous PR
    
    ---------
    
    Co-authored-by: Malak Hannosh <77818528+malakhannosh@users.noreply.github.com>
    Co-authored-by: Maya Theresa Gopal <69762402+mayather@users.noreply.github.com>
    Co-authored-by: Nathan Flinchum <89219510+nathanlf@users.noreply.github.com>
    
    * Fix design-doc merge conflict (#38)
    
    * Adding parts 0 and 1 of design-doc (#10)
    
    * Add section 4 of design doc (#11)
    
    LGTM
    
    * Add personas and user stories to design doc (#12)
    
    * Add personas and user stories to design doc
    
    * Update design doc personas to provide more detail
    
    * Add section 5 to Design-doc (#13)
    
    * Add section 5 to Design-doc
    
    * Add revised section 5 to design-doc
    
    * Create room-manage component
    
    * Fix app module import statement for room and seat
    
    * Remove old TODO file
    
    * Fix Design-doc error from previous PR
    
    * Fix Design-doc error
    
    ---------
    
    Co-authored-by: Malak Hannosh <77818528+malakhannosh@users.noreply.github.com>
    Co-authored-by: Maya Theresa Gopal <69762402+mayather@users.noreply.github.com>
    Co-authored-by: Nathan Flinchum <89219510+nathanlf@users.noreply.github.com>
    
    * Moved seat and room models to their directories (#40)
    
    * Creating room page component. (#42)
    
    * Creating room page component.
    
    * Commenting in potential HTML changes.
    
    I couldn't yet display information in the html as we haven't implemented
    the component yet, but have created a guide for when that implementation
    is possible.
    
    * Added rooms view to navigation sidebar, Pair Programming (#45)
    
    * Populating seat service with methods. (#46)
    
    * Called seat-service in seat component (#49)
    
    ---------
    
    Co-authored-by: Maya Theresa Gopal <mayather@email.unc.edu>
    Co-authored-by: Maya Theresa Gopal <69762402+mayather@users.noreply.github.com>
    Co-authored-by: Malak Hannosh <77818528+malakhannosh@users.noreply.github.com>
    Co-authored-by: Aknazar Janibek <89208624+ajanibekcode@users.noreply.github.com>

--- frontend/src/app/seat/seat-page/seat-page.component.ts
+++ frontend/src/app/seat/seat-page/seat-page.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Seat } from '../seat.model';
+import { ActivatedRoute } from '@angular/router';
+import { SeatService } from '../seat.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-seat-page',
@@ -6,5 +10,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./seat-page.component.css']
 })
 
-export class SeatPageComponent {}
-
+export class SeatPageComponent {
+  public seats: Observable<Seat[]>;
+  constructor(
+    private route: ActivatedRoute,
+    private seatService: SeatService
+  ) {
+    /** Initialize data from resolvers. */
+    const data = this.route.snapshot.data as {
+      seats: Seat[];
+    };
+    this.seats = seatService.getSeats();
+  }
+}
