4a7f25e9ba801dd0acee9e5cceb6fd9563ee2226
Maya Theresa Gopal <69762402+mayather@users.noreply.github.com>
2023-11-10 01:02:03 -0500

    Fix method names and add comments (#57)
    * Fix method names and add comments
    
    * Make snackBar a comment

--- frontend/src/app/room/room.service.ts
+++ frontend/src/app/room/room.service.ts
@@ -3,29 +3,46 @@ import { HttpClient } from '@angular/common/http';
 import { AuthenticationService } from '../authentication.service';
 import { Observable } from 'rxjs';
 import { Room } from './room.model';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RoomService {
   constructor(
+    //protected snackBar: MatSnackBar
     protected http: HttpClient,
     protected auth: AuthenticationService
   ) {}
 
+  /** Returns all room entries from the backend database table using the backend HTTP get request.
+   * @returns {Observable<Room[]>}
+   */
   getRooms(): Observable<Room[]> {
     return this.http.get<Room[]>('/api/rooms');
   }
 
+  /** Returns the room object from the backend database table using the backend HTTP get request.
+   * @param id: String representing the organization id
+   * @returns {Observable<Room[]>}
+   */
   getRoom(id: String): Observable<Room> {
     return this.http.get<Room>('/api/rooms/' + id);
   }
 
-  createSeat(room: Room): Observable<Room> {
+  /** Returns the new room object from the backend database table using the backend HTTP post request.
+   * @param room: RoomSummary representing the new room
+   * @returns {Observable<Room>}
+   */
+  createRoom(room: Room): Observable<Room> {
     return this.http.post<Room>('/api/rooms', room);
   }
 
-  updateSeat(room: Room): Observable<Room> {
+  /** Returns the updated room object from the backend database table using the backend HTTP put request.
+   * @param room: RoomSummary representing the updated room
+   * @returns {Observable<Room>}
+   */
+  updateRoom(room: Room): Observable<Room> {
     return this.http.put<Room>('/api/rooms', room);
   }
 }
