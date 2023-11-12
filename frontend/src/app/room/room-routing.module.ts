ceaf670d1b698490abeb24b946e3d394b8113864
Nathan Flinchum <89219510+nathanlf@users.noreply.github.com>
2023-11-12 15:27:41 -0500

    29 page navigation (#62)
    * Added rooms view to navigation sidebar, Pair Programming
    
    * Connect room page component to navigation sidebar and rename tab from 'Rooms View' to 'Rooms'
    
    * Add public fields to room page component (#61)
    
    * Add public fields to room page component
    
    * Add todo comments for create/delete room methods
    
    * Update room page title to reduce redundancy and improve clarity slightly
    
    * Update room page title to improve clarity slightly
    
    * Update room page title to clarify the current page
    
    ---------
    
    Co-authored-by: Maya Theresa Gopal <69762402+mayather@users.noreply.github.com>

--- frontend/src/app/room/room-routing.module.ts
+++ frontend/src/app/room/room-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RoomPageComponent } from './room-page/room-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [RoomPageComponent.Route];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
