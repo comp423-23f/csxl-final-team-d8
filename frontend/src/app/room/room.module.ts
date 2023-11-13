4e61595b68a0c63f44cf3c0a5bbfc1e282aacbfb
Maya Theresa Gopal <mayather@email.unc.edu>
2023-11-12 22:26:31 -0500

    Attempt form for rooms, not working

--- frontend/src/app/room/room.module.ts
+++ frontend/src/app/room/room.module.ts
@@ -6,6 +6,20 @@ import { RoomManageComponent } from './room-manage/room-manage.component';
 import { RoomPageComponent } from './room-page/room-page.component';
 
 import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
 /* UI Widgets */
 import { RoomCard } from './widgets/room-card.widget';
 import { SharedModule } from '../shared/shared.module';
