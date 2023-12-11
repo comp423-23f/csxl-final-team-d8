50c2f37571fb2b7b99d57eb936a98e6d6d7ff201
Malak Hannosh <77818528+malakhannosh@users.noreply.github.com>
2023-12-10 21:10:00 -0500

    Removed add/ delete seat functionality. (#114)

--- frontend/src/app/seat/seat.service.ts
+++ frontend/src/app/seat/seat.service.ts
@@ -19,37 +19,4 @@ export class SeatService {
   getSeats(id: String): Observable<Seat[]> {
     return this.http.get<Seat[]>('/api/seats/' + id);
   }
-
-  /** Returns the seat object from the backend database table using the backend HTTP get request.
-   * @param title: String representing the seat's name
-   * @returns {Observable<Seat[]>}
-   */
-  getSeat(title: String): Observable<Seat> {
-    return this.http.get<Seat>('/api/seats/' + title);
-  }
-
-  /** Returns the new seat object from the backend database table using the backend HTTP post request.
-   * @param seat: SeatSummary representing the new seat
-   * @returns {Observable<Seat>}
-   */
-  createSeat(seat: Seat): Observable<Seat> {
-    return this.http.post<Seat>('/api/seats', seat);
-  }
-
-  /** Returns the updated seat object from the backend database table using the backend HTTP put request.
-   * @param seat: SeatSummary representing the updated room
-   * @returns {Observable<Seat>}
-   */
-  updateSeat(seat: Seat): Observable<Seat> {
-    return this.http.put<Seat>('/api/seats', seat);
-  }
-
-  /** Deletes a seat
-   * @param seat: seat object to delete
-   * @returns {Observable<Seat>}
-   */
-
-  deleteSeat(seat: Seat): Observable<Seat> {
-    return this.http.delete<Seat>('/api/rooms/' + seat.title);
-  }
 }
